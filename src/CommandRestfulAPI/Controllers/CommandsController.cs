66e3cbb26bf3f8f6f27c2c0e3ddf9a897a8aaca2
xernai <montcorona@gmail.com>
2020-03-28 12:06:26 -0600

    Adding XML comments in CommandsController

--- src/CommandRestfulAPI/Controllers/CommandsController.cs
+++ src/CommandRestfulAPI/Controllers/CommandsController.cs
@@ -1,77 +1,81 @@
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using CommandRestfulAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace CommandRestulAPI.Controllers
 {
+    /// <resumen>
+    /// Endpoints
+    /// </resumen>
+
     [Route("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        private readonly CommandContext _context; 
-        public CommandsController(CommandContext context) 
-        { 
-            _context = context; 
-        } 
+        private readonly CommandContext _context;
+        public CommandsController(CommandContext context)
+        {
+            _context = context;
+        }
 
-        [HttpGet] 
-        public ActionResult<IEnumerable<Command>> GetCommandItems() 
-        { 
-            return _context.CommandItems; 
-        } 
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetCommandItems()
+        {
+            return _context.CommandItems;
+        }
 
-        [HttpGet("{id}")] 
-        public ActionResult<Command> GetCommandItem(int id) 
-        { 
-            var commandItem = _context.CommandItems.Find(id); 
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandItem(int id)
+        {
+            var commandItem = _context.CommandItems.Find(id);
 
-            if(commandItem == null) 
-            { 
-                return NotFound(); 
-            } 
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
 
-            return commandItem; 
-        } 
+            return commandItem;
+        }
 
-        [HttpDelete("{id}")] 
-        public ActionResult<Command> DeleteCommandItem(int id) 
-        { 
-            var commandItem = _context.CommandItems.Find(id); 
+        [HttpDelete("{id}")]
+        public ActionResult<Command> DeleteCommandItem(int id)
+        {
+            var commandItem = _context.CommandItems.Find(id);
 
-            if(commandItem == null) 
-            { 
-                return NotFound(); 
-            } 
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
 
-            _context.CommandItems.Remove(commandItem); 
+            _context.CommandItems.Remove(commandItem);
             _context.SaveChanges(); // importante para el DELETE
 
-            return commandItem; 
-        } 
+            return commandItem;
+        }
 
-        [HttpPost] 
-        public ActionResult<Command> PostCommandItem(Command command) 
-        { 
-            _context.CommandItems.Add(command); 
+        [HttpPost]
+        public ActionResult<Command> PostCommandItem(Command command)
+        {
+            _context.CommandItems.Add(command);
             _context.SaveChanges(); // importante para el UPDATE
 
-            return CreatedAtAction("GetCommandItem", new Command{Id = command.Id}, command); 
-        } 
+            return CreatedAtAction("GetCommandItem", new Command { Id = command.Id }, command);
+        }
 
-        [HttpPut("{id}")] 
-        public ActionResult<Command> PutCommandItem(int id, Command command) 
-        { 
-            if(id != command.Id) 
-            { 
-                return BadRequest(); 
-            } 
-        
-            _context.Entry(command).State = EntityState.Modified; 
-            _context.SaveChanges(); 
+        [HttpPut("{id}")]
+        public ActionResult<Command> PutCommandItem(int id, Command command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
 
-            return NoContent(); 
-        } 
+            _context.Entry(command).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
 
         // [HttpGet]
         // public ActionResult<IEnumerable<string>> Auladiser()
